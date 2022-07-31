c881b70110e7e9ea7f5f9f5ccda6c81787a09832
Julio <you@example.com>
2022-07-31 16:02:57 -0300

    dia 31/07

--- 1022-TDA-Racional-naoterminei.js
+++ 1022-TDA-Racional-naoterminei.js
@@ -4,8 +4,32 @@ var lines = input.split('\n');
 
 let removeElemento = lines.shift();
 
-let valores = lines.split(" ")
+let linha1 = lines.shift().split(' ')
+let somaN1 = parseInt(linha1[0])
+let somaD1 = parseInt(linha1[2])
+let somaN2 = parseInt(linha1[4])
+let somaD2 = parseInt(linha1[6])
 
 
-console.log(valores)
+let linha2 = lines.shift().split(' ')
+let subtracaoN1 = parseInt(linha1[0])
+let subtracaoD1 = parseInt(linha1[2])
+let subtracaoN2 = parseInt(linha1[4])
+let subtracaoD2 = parseInt(linha1[6])
+
+let linha3 = lines.shift().split(' ')
+let multiplicacaoN1 = parseInt(linha1[0])
+let multiplicacaoD1 = parseInt(linha1[2])
+let multiplicacaoN2 = parseInt(linha1[4])
+let multiplicacaoD2 = parseInt(linha1[6])
+
+let linha4 = lines.shift().split(' ')
+let divisaoN1 = parseInt(linha1[0])
+let divisaoD1 = parseInt(linha1[2])
+let divisaoN2 = parseInt(linha1[4])
+let divisaoD2 = parseInt(linha1[6])
+
+const soma = (somaN1 * somaD2 + somaN2 * somaD1)
+let calculo1 = simplify(10/8) 
+console.log(soma)
 
