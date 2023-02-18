d9e62aa3e87400b6686e5824ca88d2f8a06ceceb
Julio-Braz <juliobraz@id.uff.br>
2023-02-18 17:42:32 -0300

    18-02

--- 1181-Linha-na-Matriz.js
+++ 1181-Linha-na-Matriz.js
@@ -1,1 +1,33 @@
 let input = require('fs').readFileSync('./stdin', 'utf8');
+
+let linha = input.split('\n')
+const linhaEscolhida = parseInt(linha.shift())
+const operacaoEscolhida = linha.shift()
+const numbers = linha.map(parseFloat)
+
+let S = 0
+
+function somando(){
+
+for (let i = 0; i < 12; i++) {
+    if (linhaEscolhida == '0') {
+        S += numbers[i]
+        
+    } else {
+        S += numbers[(i + (linhaEscolhida * 12))]
+        
+    }
+} return S
+}
+
+function operacao(){
+if(operacaoEscolhida == 'S'){
+    console.log(somando().toFixed(1))
+
+}else{
+    media = (somando() /12).toFixed(1)
+    console.log(media)
+}
+}
+
+operacao()
